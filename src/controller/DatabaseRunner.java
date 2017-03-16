00982a74c9d359c8f10a210ab905a1894f3782a4
Xyrephon <jtuc1613@740s-pl-115.local>
2017-03-16 09:35:11 -0600

    Added MySQL to my database.
    This is a lot of changes.

---
+++ src/controller/DatabaseRunner.java
@@ -0,0 +1,10 @@
+package controller;
+
+public class DatabaseRunner 
+{
+	public static void main(String [] args)
+	{
+		DatabaseController appController = new DatabaseController();
+		appController.start();
+	}
+}
